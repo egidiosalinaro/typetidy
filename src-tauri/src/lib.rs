c8634e3688f80e099ace5668443ea4b2dda37820
egidiosalinaro <egy.s96@gmail.com>
2025-01-09 14:46:42 +0100

    fix: update Rust code for Tauri v1

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,20 +1,13 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
+    .setup(|_app| {
       Ok(())
     })
-    .on_window_event(|window, event| {
-      if let tauri::WindowEvent::Focused(focused) = event {
-        if *focused {
-          window.set_title("TypeTidy").unwrap();
+    .on_window_event(|e| {
+      if let tauri::WindowEvent::Focused(focused) = e.event() {
+        if focused {
+          e.window().set_title("TypeTidy").unwrap();
         }
       }
     })
